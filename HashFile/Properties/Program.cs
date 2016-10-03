bb61e6be66ffa825db82df8335347bb1b7c78d30
pau <pau@DESKTOP-4A4863B>
2016-10-03 18:03:24 -0500

    commit

--- HashFile/Properties/Program.cs
+++ HashFile/Properties/Program.cs
@@ -23,6 +23,33 @@ namespace HashFile
             String textOut = BitConverter.ToString(hashResult, 0);
 
             File.WriteAllText(@"C:\Users\pau\Documents\texthash.txt", textOut);
+
+            Console.WriteLine("Quin arxiu vols encriptar? Posa la ruta.");
+
+            String arxiu = Console.ReadLine();
+
+            arxiu = File.ReadAllText(arxiu);
+
+            byte[] bytesArxiu = UTF8Encoding.UTF8.GetBytes(arxiu);
+
+            byte[] hashResultat = SHA512.ComputeHash(bytesArxiu);
+
+            Console.WriteLine("Selecciona el arxiu hash a comparar, posa la ruta:");
+
+            String hash = Console.ReadLine();
+
+            hash = File.ReadAllText(hash);
+
+            if(arxiu == hash)
+            {
+                Console.WriteLine("Els arxius coincideixen!!");
+
+            }
+            else
+            {
+                Console.WriteLine("No coincideixen!!");
+            }
+
         }
     }
 }
