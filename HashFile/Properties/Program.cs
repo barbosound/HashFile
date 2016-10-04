039a71e609e85290bd2b7cc14033961290ec74e0
pau <pau@DESKTOP-4A4863B>
2016-10-03 19:30:18 -0500

    commit

--- HashFile/Properties/Program.cs
+++ HashFile/Properties/Program.cs
@@ -12,44 +12,67 @@ namespace HashFile
     {
         static void Main(string[] args)
         {
-            String contingut = File.ReadAllText(@"C:\Users\pau\Documents\text.txt");
 
-            byte[] bytesIn = UTF8Encoding.UTF8.GetBytes(contingut);
+            int opcio;
 
-            SHA512Managed SHA512 = new SHA512Managed();
+            Console.WriteLine("     MENÚ");
+            Console.WriteLine("[1]GENERAR HASH");
+            Console.WriteLine("[2]COMPARAR HASH");
 
-            byte[] hashResult = SHA512.ComputeHash(bytesIn);
+            opcio = Console.Read();
 
-            String textOut = BitConverter.ToString(hashResult, 0);
-
-            File.WriteAllText(@"C:\Users\pau\Documents\texthash.txt", textOut);
-
-            Console.WriteLine("Quin arxiu vols encriptar? Posa la ruta.");
-
-            String arxiu = Console.ReadLine();
-
-            arxiu = File.ReadAllText(arxiu);
-
-            byte[] bytesArxiu = UTF8Encoding.UTF8.GetBytes(arxiu);
-
-            byte[] hashResultat = SHA512.ComputeHash(bytesArxiu);
-
-            Console.WriteLine("Selecciona el arxiu hash a comparar, posa la ruta:");
-
-            String hash = Console.ReadLine();
-
-            hash = File.ReadAllText(hash);
-
-            if(arxiu == hash)
+            if(opcio == '1')
             {
-                Console.WriteLine("Els arxius coincideixen!!");
+                String contingut = File.ReadAllText(@"C:\Users\pau\Documents\text.txt");
+
+                byte[] bytesIn = UTF8Encoding.UTF8.GetBytes(contingut);
+
+                SHA512Managed SHA512 = new SHA512Managed();
+
+                byte[] hashResult = SHA512.ComputeHash(bytesIn);
+
+                String textOut = BitConverter.ToString(hashResult, 0);
+
+                File.WriteAllText(@"C:\Users\pau\Documents\texthash.txt", textOut);
 
             }
-            else
+            if(opcio == '2')
             {
-                Console.WriteLine("No coincideixen!!");
-            }
+                Console.WriteLine("Quin arxiu vols encriptar? Posa la ruta.");
 
+                String ruta = Console.ReadLine();
+
+                String arxiu = File.ReadAllText(ruta);
+
+                byte[] bytesArxiu = UTF8Encoding.UTF8.GetBytes(arxiu);
+
+                SHA512Managed SHA512 = new SHA512Managed();
+
+                byte[] hashResultat = SHA512.ComputeHash(bytesArxiu);
+
+                String resultat = BitConverter.ToString(hashResultat, 0);
+
+                arxiu = resultat;
+
+                Console.WriteLine("Selecciona el arxiu hash a comparar, posa la ruta:");
+
+                String hash = Console.ReadLine();
+
+                hash = File.ReadAllText(hash);
+
+                if (arxiu == hash)
+                {
+                    Console.WriteLine("Els arxius coincideixen!!");
+                    Console.ReadKey();
+
+                }
+                else
+                {
+                    Console.WriteLine("No coincideixen!!");
+                    Console.ReadKey();
+                }
+
+            }         
         }
     }
 }
